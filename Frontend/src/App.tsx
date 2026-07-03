f325b589b2e52c6156797f14999801e5ed13e779
praveen Fernando <praveenfdo03@gmail.com>
2026-07-03 10:43:58 +0530

    feat: add Home component and update index.html references

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./pages/Navbar/Navbar.jsx";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-50 text-slate-900">
       <Navbar />
+      <Home />
     </div>
   );
 }
